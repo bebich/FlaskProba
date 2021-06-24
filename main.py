9f5a65d5e3e788b5e2e2156592f7603a467d7e95
fbebek <finassistapp@gmail.com>
2021-06-24 18:49:34 +0200

    da

--- main.py
+++ main.py
@@ -2,11 +2,3 @@ from flask import render_template
 from app import app
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
-
-
-@app.route('/Filip')
-def hello_filip():
-    return render_template("filip.html")
